250ce8f6a66f4ca63988056ab178d812115e2fb7
kaushik <kaushiksanil4@gmail.com>
2025-10-12 00:13:12 +0530

    deployment update

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,21 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Change this if deploying to a subdirectory
   server: {
     port: 3000,
     open: true
   },
   build: {
     outDir: 'dist',
-    sourcemap: true
+    sourcemap: false, // Disable for production
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'crypto': ['crypto-js']
+        }
+      }
+    }
   }
 })
